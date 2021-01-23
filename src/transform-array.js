a7e2fa958eeb6fb8c388046f2f5df383a54e46a8
Leonid Ionov <neonych@gmail.com>
2021-01-24 01:27:24 +0300

    feat: implement transform(arr) function

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,6 +1,24 @@
-const CustomError = require("../extensions/custom-error");
-
-module.exports = function transform(/* arr */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = (arr) => {
+    if (Object.prototype.toString.call(arr) === '[object Array]') {
+        const copyArr = [...arr]
+        return copyArr.reduce((newArr,item,i,arr) => {
+                switch (item) {
+                    case '--discard-next' :
+                        arr[i+1] = null
+                        return newArr
+                    case '--discard-prev' :
+                        newArr.pop()
+                        return newArr
+                    case '--double-next' :
+                        if (arr[i+1] !== undefined) newArr.push(arr[i+1])
+                        return newArr
+                    case '--double-prev' :
+                        if (arr[i-1] !== undefined) newArr.push(arr[i-1])
+                        return newArr
+                    default :
+                        newArr.push(item)
+                        return newArr
+                }
+            },[]).filter(item => item !== null)
+    } else {throw new Error()}
 };
