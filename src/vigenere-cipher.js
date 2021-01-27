943cb3ac8fc6cd6dd6b07af5d189fb19b59fc736
Leonid Ionov <neonych@gmail.com>
2021-01-28 02:13:57 +0300

    feat: implement VigenereCipheringMachine class

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -1,14 +1,44 @@
-const CustomError = require("../extensions/custom-error");
+const VigenereCipheringMachine = (() => {
+    const _privateProps = new WeakMap(),
+        _privateMethods = {
+            equalKeyToString: (str, key, thisClass) => {
+                let count = 0,
+                    newKey = [...str.toUpperCase()].map(item => {
+                        if (/[A-Z]/.test(item)) {
+                            item = key[((count >= key.length) ? count % key.length : count)]
+                            count++
+                            return item
+                        } else return item
+                    }).join('').toUpperCase()
+                return _privateProps.set(thisClass, {keyToString: newKey})
+            }
+        }
 
-class VigenereCipheringMachine {
-  encrypt() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
-  }    
-  decrypt() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
-  }
-}
+    class VigenereCipheringMachine {
+        constructor(reverse = true) {
+            this.reverseMachine = reverse
+        }
 
-module.exports = VigenereCipheringMachine;
+        encrypt(str, key) {
+            _privateMethods.equalKeyToString(str, key, this)
+            const keyToString = _privateProps.get(this).keyToString,
+                encryptString = [...str.toUpperCase()].map((item, i) => /[A-Z]/.test(item) ?
+                    String.fromCharCode((item.charCodeAt(0) + keyToString.charCodeAt(i)) % 26 + 65) :
+                    item)
+            return this.reverseMachine ? encryptString.join('') : encryptString.reverse().join('')
+        }
+
+        decrypt(str, key) {
+            _privateMethods.equalKeyToString(str, key, this)
+            const keyToString = _privateProps.get(this).keyToString,
+                decryptString = [...str.toUpperCase()].map((item, i) => /[A-Z]/.test(item) ?
+                    String.fromCharCode((item.charCodeAt(0) - keyToString.charCodeAt(i) + 26) % 26 + 65) :
+                    item)
+            return this.reverseMachine ? decryptString.join('') : decryptString.reverse().join('')
+        }
+    }
+
+    return VigenereCipheringMachine
+})()
+
+module.exports = VigenereCipheringMachine
